a504387cd0f6c3164e48513ee48318f029a27721
acesarfc <afloresca@uoc.edu>
2025-12-13 20:55:23 +0100

    Need to finish course jpa ops

--- autumn-2025-course/src/main/java/edu/uoc/epcsd/course/infrastructure/repository/jpa/CourseRepositoryImpl.java
+++ autumn-2025-course/src/main/java/edu/uoc/epcsd/course/infrastructure/repository/jpa/CourseRepositoryImpl.java
@@ -1,15 +1,17 @@
 package edu.uoc.epcsd.course.infrastructure.repository.jpa;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import edu.uoc.epcsd.course.domain.Course;
 import edu.uoc.epcsd.course.domain.exception.UserNotFoundException;
 import edu.uoc.epcsd.course.domain.repository.CourseRepository;
 import edu.uoc.epcsd.course.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -35,17 +37,22 @@ public class CourseRepositoryImpl implements CourseRepository {
     	if (!userRepository.findUserByEmail(course.getInstructor())) {
             throw new UserNotFoundException(course.getInstructor());
         }
-	    // TODO: add the code for the missing system operations here 
 		// create CourseEntity, set instructor and status and return your id
-        
-        return null; 
+		CourseEntity courseEntity = CourseEntity.fromDomain(course);
+
+		if (courseEntity != null){
+			return jpaCourseRepository.save(courseEntity).getId();
+		}
+        return null;
     }
 
 	@Override
 	public Long modifyCourseDetails(Course course) {
         
-	    // TODO: add the code for the missing system operations here: 
 		// If instructor exists, set all course variables, save CourseEntity and return your id
+		if (!userRepository.findUserByEmail(course.getInstructor())) {
+            throw new UserNotFoundException(course.getInstructor());
+        }
 
         return null;  
 	}
