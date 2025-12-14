fd05e1b4911c662354f9f04943877333595c1612
acesarfc <afloresca@uoc.edu>
2025-12-14 20:27:49 +0100

    event mock mail added

--- autumn-2025-notification/src/main/java/edu/uoc/epcsd/notification/domain/service/NotificationServiceImpl.java
+++ autumn-2025-notification/src/main/java/edu/uoc/epcsd/notification/domain/service/NotificationServiceImpl.java
@@ -1,16 +1,17 @@
 package edu.uoc.epcsd.notification.domain.service;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import edu.uoc.epcsd.notification.application.kafka.CourseMessage;
 import edu.uoc.epcsd.notification.application.kafka.KafkaConstants;
 import edu.uoc.epcsd.notification.application.kafka.ProductMessage;
 import edu.uoc.epcsd.notification.application.rest.dtos.GetProductResponse;
 import edu.uoc.epcsd.notification.application.rest.dtos.GetUserResponse;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.time.LocalDate;
 
 @Log4j2
 @Service
@@ -51,6 +52,13 @@ public class NotificationServiceImpl implements NotificationService {
                 "MOCK EMAIL: Enrollment has been opened for course {}",
                 courseMessage.getCourseId()
             );
+            break;
+            
+        case KafkaConstants.COURSE_ENROLLMENT_CLOSED:
+            log.info(
+                "MOCK EMAIL: Enrollment has been closed for course {}",
+                courseMessage.getCourseId()
+            ); 
             break;
 
         case KafkaConstants.COURSE_STUDENT_ENROLLED:
